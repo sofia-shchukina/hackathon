897ed1099c90171996e558e66733d940602c91f1
melikeo <98155324+melikeo@users.noreply.github.com>
2023-07-07 20:54:32 +0200

    folder structure

---
+++ backend/src/main/java/com/example/hackathon/SocialShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialShoppingApplication.class, args);
+	}
+
+}
---
+++ frontend/android/app/src/main/kotlin/com/example/herhackathon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.herhackathon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
